305f9bef70b0167032f88795102242003887366c
Collins Mutai <collinsfrontend@gmail.com>
2025-12-02 00:23:34 +0300

    add admin dashboard

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 const corsOptions = {
   origin: [
     "http://localhost:3000", // Local frontend
-    "https://nonforbearing-semiadhesively-isadora.ngrok-free.dev", // Ngrok URL for backend
+    "http://localhost:5000", // Ngrok URL for backend
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
