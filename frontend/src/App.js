962b80a740a871c0c33a944cf223edfcb9e41a70
Collins Mutai <collinsfrontend@gmail.com>
2025-12-11 16:15:18 +0300

    v1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,15 +22,6 @@ function App() {
   const dispatch = useDispatch();
   const { isLoggedIn, user } = useSelector((state) => state.user);
 
-  // Function to check if the user is logged in
-  const checkUserRole = (requiredRole) => {
-    if (!isLoggedIn) return <Navigate to="/login" />;
-    if (user?.role !== requiredRole) {
-      return <Navigate to="/bookings" />; // Redirect non-admin users to bookings page
-    }
-    return true; // Allow access if the role matches
-  };
-
   // Handle the logic when the app loads to fetch the user info (e.g., from localStorage or API)
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -38,7 +29,7 @@ function App() {
     if (token && storedUser) {
       const user = JSON.parse(storedUser);
       dispatch(setUser(user)); // Update the redux store with the user
-      dispatch(login({ user, token })); // Make sure to update the login state
+      dispatch(login({ user, token })); // Update the login state
     }
   }, [dispatch]);
 
@@ -50,12 +41,17 @@ function App() {
         <Route path="/places" element={<Places />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/paypalpayment/success" element={<PaypalPaymentSuccess />} />
-        <Route path="/bookings" element={user?.role === "admin" ? <Navigate to="/admin" /> : <Bookings />} />
+
+        {/* This route checks for admin and redirects accordingly */}
+        <Route
+          path="/bookings"
+          element={user?.role === "admin" ? <Navigate to="/admin" /> : <Bookings />}
+        />
 
         {/* Protect the admin route */}
         <Route
           path="/admin"
-          element={user?.role === "admin" ? <AdminDashboard /> : <Navigate to="/bookings" />}
+          element={user?.role === "admin" ? <AdminDashboard /> : <Navigate to="/" />}
         />
       </Routes>
       <Footer />
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -323,7 +323,7 @@ const Navbar = () => {
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
             </svg>
-            <span>+1 234 567 890</span>
+            <span>+17194920042</span>
           </div>
 
           <div className="book-now">
